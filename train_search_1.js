8284369145ee61484d5e5f284c619ceb018b0434
Naresh Kumar H <nareshkumarh@live.com>
2021-01-28 12:20:15 +0530

    Update and rename train_search.js to train_search_1.js

---
+++ train_search_1.js
@@ -0,0 +1,32 @@
+const trains = [
+    { trainNo:123, from:"Chennai Central",to: "Bangalore" },
+    { trainNo:124, from:"Chennai Central",to: "Bangalore" },
+    { trainNo:125, from:"Chennai Central",to: "Madurai"},
+    { trainNo:126, from:"Chennai Central",to: "Bangalore"},
+];
+
+const cancelledTrains = [ 
+    {trainNo: 123, cancelledOn:"2021-01-28"}
+];
+
+/** 
+Find trains based on source and destination.
+Find trains based on the journeyDate ( exclude cancelledTrains
+*/
+
+function searchTrains(source, destination, journeyDateStr){
+
+    let journeyDate = new Date(journeyDateStr);            
+    let filteredTrains = [] ;
+    // implement logic
+    return filteredTrains;
+}
+
+// Test Case #1: 
+let availableTrains1= searchTrains("Chennai Central","Bangalore","2020-01-27");
+console.log("List Available Trains - Test Case #1");
+console.log(availableTrains1);
+
+let availableTrains2= searchTrains("Chennai Central","Bangalore","2020-01-28");
+console.log("List Available Trains - Test Case #2");
+console.log(availableTrains2);
